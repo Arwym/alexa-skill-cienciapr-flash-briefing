b0ebaea368a010c12cfac0a5c492b0030e045681
Jessica M <contact@jmvazquez.com>
2018-01-02 21:13:00 -0400

    Updated to fetch feed from CienciaPR.org and cleanup description field for reading.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const rp = require('request-promise');
+const striptags = require('striptags');
+const rmNewline = require('newline-remove');
 const xml2js = require('xml2js');
 const express = require('express');
 const dateFormat = require('dateformat');
 const app = express();
 
-const RSS_URL = 'http://www.animenewsnetwork.com/all/rss.xml';
-const RSS_POLL_TIMEOUT = 15 * 60 * 1000
+const RSS_URL = 'https://www.cienciapr.org/en/feeds/news.xml?field_cienciapr_collaboration_value=All';
+const RSS_POLL_TIMEOUT = 24 * 60 * 1000;
 let dataCache = {};
 
 app.set('port', process.env.PORT || 3000);
@@ -15,7 +17,7 @@ app.get('/', (appReq, appRes) => {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('RSS proxy running on port', app.get('port'));
+  console.log('CienciaPR.org RSS proxy running on port', app.get('port'));
 });
 
 function updateDataCache() {
@@ -27,11 +29,13 @@ function updateDataCache() {
         rssJSON.rss.channel[0].item.forEach(item => {
           const itemDate = new Date(item.pubDate[0]);
 
+          console.log(item.title[0]);
+
           alexaNewsItems.push({
             uid: item.guid[0]._,
             updateDate: dateFormat(itemDate, `UTC:yyyy-mm-dd'T'HH:MM:ss'.0Z'`),
             titleText: item.title[0],
-            mainText: item.description[0],
+            mainText: rmNewline(striptags(item.description[0])).trim(),
             redirectionURL: item.link[0]
           });
         });
